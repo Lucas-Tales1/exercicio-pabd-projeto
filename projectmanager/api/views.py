adb69c633a752a77c4f45f42e64f5d8f98f91f61
MANOEL PINTO JUNIOR <manoel.pinto@escolar.ifrn.edu.br>
2025-10-29 10:11:49 -0300

    feat: criar UsuarioViewSet

--- projectmanager/api/views.py
+++ projectmanager/api/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Projeto, Tarefa
-from .serializers import ProjetoSerializer, TarefaSerializer
+from .models import Usuario, Projeto, Tarefa
+from .serializers import UsuarioSerializer, ProjetoSerializer, TarefaSerializer
 
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import status
 
 # Create your views here.
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
 
 class ProjetoViewSet(viewsets.ModelViewSet):
     queryset = Projeto.objects.all()
