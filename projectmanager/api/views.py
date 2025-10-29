07b453e6243854bec00b263d075775a2f4189ce5
MANOEL PINTO JUNIOR <manoel.pinto@escolar.ifrn.edu.br>
2025-10-29 10:01:53 -0300

    feat: adicionar viewsets dos modelos Projeto e Tarefa

--- projectmanager/api/views.py
+++ projectmanager/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Projeto, Tarefa
+from .serializers import ProjetoSerializer, TarefaSerializer
+
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import status
 
 # Create your views here.
+
+class ProjetoViewSet(viewsets.ModelViewSet):
+    queryset = Projeto.objects.all()
+    serializer_class = ProjetoSerializer 
+
+
+
+class TarefaViewSet(viewsets.ModelViewSet):
+    queryset = Tarefa.objects.all()
+    serializer_class = TarefaSerializer 
+
