f6c531c7b0fc4d0ce2c54fe86c31655070cc48b7
LUCAS TALES BERNARDO BARBOSA <20241014040018@ifrn.local>
2025-10-29 09:27:42 -0300

    feat: criar models

--- projectmanager/api/models.py
+++ projectmanager/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Usuario(AbstractUser):
+    nome = models.CharField(max_length=50)
+
+class Projeto(models.Model):
+    usuario = models.ForeingKey()
+    nome = models.CharField(max_length=50)
+
+class Tarefa(models.Model):
+    projeto = models.ForeignKey(Projeto)
+    nome = models.CharField(max_length=50)
